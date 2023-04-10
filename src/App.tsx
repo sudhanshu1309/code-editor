4f1992cb8b100a1bb97d204ae9e014ed5dbb47ae
Sudhanshu Tripathi <sudhanshutripathi008@gmail.com>
2023-04-10 23:18:06 +0530

    Initialised a Vite app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+  return (
+    <>
+      <div>Hello World!</div>
+    </>
+  );
+};
+
+export default App;
